41314091f922faaa0a011847774a67193c2c32df
yangdepu <yangdepp@163.com>
2019-08-30 18:16:04 +0800

    fix login bug lazy router

--- web-client/src/page/App.jsx
+++ web-client/src/page/App.jsx
@@ -1,20 +1,31 @@
-import React, { Fragment } from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
-
-import Home from './home';
-import Sign from './sign';
-import Manage from './manage';
+import React, { Suspense, lazy } from 'react';
+import './style.scss';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
+import Loading from './components/loading';
+
+const Home = lazy(() => import('./home'));
+const Sign = lazy(() => import('./sign'));
+const Manage = lazy(() => import('./manage'));
+
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <Fragment>
-          <PrivateRoute exact path="/" component={Home} />
-          <PrivateRoute exact path="/manage" component={Manage} />
-          <Route exact path="/signin" component={Sign} />
-          <Route exact path="/signup" component={Sign} />
-        </Fragment>
+        <Suspense
+          fallback={
+            <div className="loading">
+              <Loading />
+            </div>
+          }
+        >
+          <Switch>
+            <PrivateRoute exact path="/" component={Home} />
+            <PrivateRoute exact path="/manage" component={Manage} />
+            <Route exact path="/signin" component={Sign} />
+            <Route exact path="/signup" component={Sign} />
+          </Switch>
+        </Suspense>
       </Router>
     );
   }
